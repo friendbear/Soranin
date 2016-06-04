ac63e2d2acf34d4fa6d369c3930ff9042c006b3f
熊谷友博 <kumagai@bear.plala.or.jp>
2016-06-04 13:26:20 +0900

    クロージャ、部分関数、カリー化のコード

--- src/Example2.scala
+++ src/Example2.scala
@@ -41,8 +41,53 @@ object Example2 extends App{
 
   // クロージャ
   def cloger(max: Int) = (default: Int) => max + default
-  val cl = cloger(10)
+  val cl = cloger(10) // 後からdefaultの値を設定して変数化
   println(cl(2))
 
+  // 部分関数(partialFunctionトレイト)
+
+  val base: PartialFunction[Throwable, String] = {
+    case _ : IllegalArgumentException => "Parameter is invalid"
+    case _ : IllegalStateException => "State is invalid"
+  }
+  val ex: PartialFunction[Throwable, String] = {
+    case _ : RuntimeException => "Other Exception"
+  }
+  def checkMinus(i: Int): String = {
+    try {
+      if (i < 0) throw new IllegalArgumentException
+      "success"
+    } catch base orElse ex // orElseで部分関数を連結できる
+  }
+  println(checkMinus(-1))
+  println(checkMinus(1))
+  println(base.isDefinedAt(new IllegalAccessError))
+
+  // カリー化について 関数のチェーン
+  val greetFunc = (title: String) => (name: String) => title + name
+  println(greetFunc("Hello")("kumagai"))
+  def greetDef(title: String)(name: String): String = title + name
+  println(greetDef("Hello")("sorataka"))
+
+  val nameFunc = greetFunc("Goodby")
+  println(nameFunc("kumagai"))
+
+  def executeGreet(data: List[String], f: List[String] => String) = {
+    f(data)
+  }
+  // 上記をカリー化した場合
+  def executeGreetCurry(data: List[String])(f: List[String] => String) = {
+    f(data)
+  }
+  val nameList = List("kumagai", "sorataka", "nami")
+  println(executeGreetCurry(nameList){data: List[String] =>
+      data.toString()
+  })
+
+  //関数をカリー化
+  //val curryFunc = greetFunc.curried // // TODO: curried がみつからない
+
+  //メソッドをカリー化
+  //val curryDef = (greetDef _).curried // TODO: curried がみつからない
 
 }
