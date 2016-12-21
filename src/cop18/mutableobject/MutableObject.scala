f47ed0f916387d93c191f24847ddb561be7706c3
Tomohiro Kumagai <kumagai@bear.plala.or.jp>
2016-12-22 04:47:44 +0900

    BankAccount ミューダブルな銀行口座クラス
    —
    ### ミューダブルな銀行口座クラスの実装
    
    ensuringで結果をAssert

---
+++ src/cop18/mutableobject/MutableObject.scala
@@ -0,0 +1,31 @@
+package cop18.mutableobject
+
+/**
+  * Created by kumagai on 2016/12/22.
+  */
+class MutableObject {
+
+}
+
+class BankAccount(initialDeposit: Int = 0) {
+  private var bal: Int = initialDeposit
+  def balance: Int = bal
+  def deposit(amount: Int) = {
+    require(amount > 0)
+    bal += amount
+  }
+  def withdraw(amount: Int): Boolean = {
+    if (amount > bal) false
+    else {
+      bal -= amount
+      true
+    }
+  }
+}
+
+object BankAccountApp extends App {
+  val account = new BankAccount
+  account deposit 100000
+  account withdraw 90000 ensuring(_ == false, "引き出し失敗")
+  account withdraw 30000 ensuring(_ == false, "引き出し失敗")
+}
