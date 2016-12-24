e91e3ee024c8542e9bc69389af7f6a61b22b0330
Tomohiro Kumagai <kumagai@bear.plala.or.jp>
2016-12-25 04:26:48 +0900

    遅延評価型(lazy)
    lazy valの初期化シーケンスをトレースして理解。

--- src/cop20/abstractmembers/AbstractLesson.scala
+++ src/cop20/abstractmembers/AbstractLesson.scala
@@ -51,6 +51,26 @@ trait RationalTrait {
 
   override def toString: String = numer + "/" + denom
 }
+/** 抽象valの初期化(RationalTraitの遅延評価(lazy)版
+  * 初期化の実行純が意味を持たない関数型オブジェクトに適している。
+  * 一方、基本的に命令型に書かれているコードにはあまり適していない。
+  */
+trait LazyRationalTrait {
+  val numerArg: Int
+  val denomArg: Int
+  lazy val numer = numerArg / g // <== lazy付き移動
+  lazy val denom = denomArg / g // <== lazy付き移動
+
+  private lazy val g = {
+    require(denomArg != 0) // <== 移動
+    gcd(numerArg, denomArg)
+  }
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+
+  override def toString: String = numer + "/" + denom
+}
 object RationalTraitLessonApp extends App{
 /*
   // java.lang.IllegalArgumentException: requirement failed が発生する
@@ -60,13 +80,17 @@ object RationalTraitLessonApp extends App{
   } //.toString
   // 上記の対処方法として、事前初期化済みフィールドと遅延評価(lazy)val の２つがある
   */
+  new LazyRationalTrait { // 遅延評価(lazy)val によるトレイトの初期化
+    val denomArg = 1
+    val numerArg = 2
+  }.toString
   /*
   事前定義済みフィールド
    */
   new { // 無名クラス
     val numerArg = 2
     val denomArg = 3
-  } with RationalTrait = 1/2
+  } with RationalTrait
   object twoThirds extends { // オブジェクト定義に含まれている事前初期化済みフィールド
     val numerArg = 2
     val denomArg = 3
@@ -81,4 +105,14 @@ object RationalTraitLessonApp extends App{
       denom * that.denom
     )
   }
+
+  // lazy版
+  Demo.x
+}
+
+/** 遅延評価val lazy
+  *
+  */
+object Demo {
+  lazy val x = {println("initializing x"); "done" }
 }
