c4320d976d4b83438c696eb23a748f4f1b0e0434
Tomohiro Kumagai <kumagai@bear.plala.or.jp>
2016-12-25 11:50:04 +0900

    ファクトリーメソッド導入前コード
    Currencyにファクトリメソッドを持つのではなく、外部に出す

---
+++ src/cop20/abstractmembers/Currency.scala
@@ -0,0 +1,48 @@
+package cop20.abstractmembers
+
+/**
+  * Created by kumagai on 2016/12/25.
+  */
+
+/** Currencyのインスタンスは、ドル、ユーロ、円などの通貨を
+  * 単位とする金額を表している。
+  * この金額は計算に使えるものでなければならない
+  */
+
+/** 不完全な実装、Dollar + Euro ができてはまずい
+  *
+  */
+class CurrencyA {
+  val amount: Long
+  def designation: String
+
+  override def toString: String = amount + " " + designation
+  def + (that: CurrencyA): CurrencyA
+  def * (that: CurrencyA): CurrencyA
+}
+
+abstract class Dollar extends CurrencyA {
+  override def designation = "USD"
+}
+abstract class Euro extends CurrencyA {
+  override def designation = "ECD"
+}
+
+// --------------------
+abstract class AbstractCurrency {
+  type Currency <: AbstractCurrency
+  val amount: Long // 抽象型
+  def designation: String
+
+  def make(amount: Long): Currency  // ファクトリーメソッド
+  override def toString: String = amount + " " + designation
+  def + (that: Currency): Currency
+  def * (that: Currency): Currency
+}
+abstract class CurrencyZone {
+
+}
+abstract class Dollar extends AbstractCurrency {
+  override type Currency = Dollar
+  override def designation = "USD"
+}
